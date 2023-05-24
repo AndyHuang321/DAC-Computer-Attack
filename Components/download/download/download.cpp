c6336bcecc095f527ce124b70272aa3f674d0ff8
connorhhuang <connorhhuang@gmail.com>
2023-05-24 11:09:57 -0700

    download test

---
+++ Components/download/download/download.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    system("curl https://media.sciencephoto.com/image/z9360190/800wm --output %HOMEPATH%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\sealtest.jpg");
+}
+
